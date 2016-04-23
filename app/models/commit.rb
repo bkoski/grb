384feeb3ebf1ab456f508e95d1417ada2aab689a
Ben Koski <bkoski@nytimes.com>
2016-04-23 17:24:06 -0400

    track incoming commit webhooks for #18

---
+++ app/models/commit.rb
@@ -0,0 +1,43 @@
+class Commit
+  include Mongoid::Document
+
+  field :sha,       type: String
+  field :repo_name, type: String
+  field :branch,    type: String
+  field :message,   type: String
+  field :author,    type: String
+  field :url,       type: String
+  field :committed_at, type: DateTime
+
+  index({ repo_name: 1, committed_at: -1 })
+  index({ repo_name: 1, sha: 1 }, { unique: true })
+
+  def self.import(repo_name: nil, branch: nil, commit_data: nil)
+    commit = self.find_or_initialize_by(repo_name: repo_name, sha: commit_data.id || commit_data.sha)
+    commit.branch = branch
+
+    if commit_data.commit  # we're importing a record from the API
+      commit.url          = commit_data.html_url
+      commit.message      = commit_data.commit.message
+      commit.author       = commit_data.author ? commit_data.author.login : commit_data.commit.author.email
+      commit.committed_at = DateTime.parse(commit_data.commit.author.date)
+
+    else # we're importing a webhook
+      commit.url     = commit_data.url
+      commit.message = commit_data.message
+      commit.author  = commit_data.author.username || commit_data.author.name
+      commit.committed_at = DateTime.parse(commit_data.timestamp)
+
+    end
+
+    commit.save!
+
+    commit
+  end
+
+  def self.scrape_for_repo(repo_name: repo_name, branch: 'master', since: 24.years.ago)
+    all_commits = Github.repos.commits.list ENV['DEFAULT_GITHUB_ORG'], repo_name, since: since, sha: branch
+    all_commits.each { |c| import(repo_name: repo_name, branch: branch, commit_data: c) }
+  end
+
+end
--- lib/sqs_reader.rb
+++ lib/sqs_reader.rb
@@ -8,10 +8,18 @@ class SqsReader
       github_event = Hashie::Mash.new JSON.parse(sns_event['Message'])
 
       puts "Event: #{github_event.keys.sort.join(',')}"
-      # Right now, we only listen to issue events:
+
       if github_event.issue
         issue = Issue.ingest(github_event.repository.name, github_event.issue)
         puts "Ingested #{github_event.action} for #{github_event.repository.full_name}# #{github_event.issue.number}."
+
+      elsif github_event.commits && github_event.ref.to_s.match("refs/heads") # push event
+        github_event.commits.each do |c|
+          Commit.import(repo_name: github_event.repository.name,
+                        branch: github_event.ref.match("refs/heads/(.+)")[1],
+                        commit_data: c)
+        end
+
       end
     end
   end
