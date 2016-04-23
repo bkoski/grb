9488ae37bf466fd11860ebe69d3b937707a7636a
Ben Koski <bkoski@nytimes.com>
2016-04-23 17:37:24 -0400

    bugfix: allow title and login with periods

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
   get '/auth/github/callback', to: 'sessions#create'
 
   put '/milestones/sort'   => 'milestones#update_sort_order'
-  put '/milestones/:title' => 'milestones#update'
+  put '/milestones/:title' => 'milestones#update', title: /.+/
 
   put '/issues/sort'       => 'issues#update_sort_order'
   put '/issues/:id'        => 'issues#update'
   post '/issues'           => 'issues#create'
   
-  get '/milestones/:title' => 'boards#milestone', as: 'show_milestone'
+  get '/milestones/:title' => 'boards#milestone', as: 'show_milestone', title: /.+/
   get '/boards' => 'boards#show'
 
-  get '/~:login' => 'boards#user'
+  get '/~:login' => 'boards#user', login: /.+/
 
   get '/' => 'boards#list_milestones'
 end
