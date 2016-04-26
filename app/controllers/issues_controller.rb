95b1edf85bdaa23e2fac5b5286e8ea60a49377ea
Ben Koski <bkoski@nytimes.com>
2016-04-26 17:39:08 -0400

    For #11: pass issue creation through to github, ingest new record into local db, then set sort as requested

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -7,11 +7,41 @@ class IssuesController < ApplicationController
   end
 
   def create
-    missing_fields = [:repo_name, :title, :milestone_number].select { |attr| params[attr].blank? }
-    if missing_fields.length > 0
-      error_message = missing_fields.map(&:to_s).map(&:humanize).to_sentence + ' are missing.'
-      render(status: 400, text: error_message) and return
+    # Validate input
+    error_messages = []
+
+    error_messages << "A title must be provided." if params[:title].blank?
+    error_messages << "A repo must be selected."  if params[:repo_name].blank?
+
+    if error_messages.length > 0
+      render(status: 400, text: error_messages.join(' ')) and return
     end
+
+    # Build up request body
+    request_opts = {
+      user:  ENV['DEFAULT_GITHUB_ORG'],
+      repo:  params[:repo_name],
+      milestone: params[:milestone_number],
+      title: params[:title],
+    }
+
+    [:body, :assignee].each do |optional_field|
+      request_opts[optional_field] = params[optional_field] if params[optional_field].present?
+    end
+
+    # Create issue in Github.
+    github = Github.new oauth_token: Thread.current[:github_token]
+    new_issue_data = github.issues.create(request_opts)
+
+    # Ingest the data into local db so that it's immediately available.
+    new_issue = Issue.ingest(params[:repo_name], new_issue_data)
+
+    # Apply sort as requested
+    if params[:sort] == 'top'
+      top_issue = Issue.where(milestone_github_id: params[:milestone_github_id]).open.asc(:sort_order).first
+      new_issue.relative_sort('before', top_issue)
+    end
+
     head 200
   end
 
