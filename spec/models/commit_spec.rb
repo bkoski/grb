167c53bbf172ccd30c377bda1cac2e214a9e1e51
Ben Koski <ben.koski@gmail.com>
2016-06-26 21:42:36 -0400

    filled out spec

--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -4,16 +4,18 @@ RSpec.describe Commit, type: :model do
 
   describe "ingest" do
     before do
-      @commit_data = {
-
-      }
+      @commit_data = Hashie::Mash.new({
+        sha: '1234abc',
+        author: { username: 'test-user' },
+        timestamp: DateTime.now.iso8601,
+        message: '[Fixes #10] test message'
+      })
     end
 
-    it "updates the Milestone's contributor list when there is an assignee" do
-      @issue_data.milestone = { title: 'Test Milestone' }
-      
+    it "updates the Milestone's contributor list when there is an assignee, and the commit is linked to an issue" do      
       milestone = Milestone.create!(title: 'Test Milestone')
-      Commit.ingest(@commit_data)
+      issue = Issue.create!(repo_name: 'test-repo', number: 10, milestone: 'Test Milestone')
+      Commit.import(repo_name: 'test-repo', branch: 'master', commit_data: @commit_data)
     
       milestone.reload
       milestone.contributors.should == ['test-user']
